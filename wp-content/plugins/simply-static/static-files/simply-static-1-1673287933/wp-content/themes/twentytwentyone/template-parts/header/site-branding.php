48282f065be05e74dc8434b166cd903c3f343e59
Zubairkerala <120552760+Zubairkerala@users.noreply.github.com>
2023-01-09 23:58:31 +0530

    second commit

---
+++ wp-content/plugins/simply-static/static-files/simply-static-1-1673287933/wp-content/themes/twentytwentyone/template-parts/header/site-branding.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function get_bloginfo() in C:\xampp\htdocs\school5\wp-content\themes\twentytwentyone\template-parts\header\site-branding.php:10
+Stack trace:
+#0 {main}
+  thrown in <b>C:\xampp\htdocs\school5\wp-content\themes\twentytwentyone\template-parts\header\site-branding.php</b> on line <b>10</b><br>
